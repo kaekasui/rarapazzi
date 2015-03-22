fbc2ebce448f284e06be7656eb1720d4c49f933b
lysaco <risako908@gmail.com>
2015-03-22 17:43:36 +0900

    場所追加したお

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -1,5 +1,20 @@
 class ExampleController < ApplicationController
   def index
- #35.632896.139.880394
+    @events = Event.all
+
+    @events = Event.all
+
+    @hash = Gmaps4rails.build_markers(@events) do |event, marker|
+      marker.lat event.latitude
+      marker.lng event.longitude
+      marker.infowindow event.description
+      marker.picture({
+        "url" => "https://addons.cdn.mozilla.net/img/uploads/addon_icons/13/13028-64.png",
+        "width" => "32",
+        "height" => "32"
+        })
+      marker.json({ title: event.title })
+      
+    end
   end
 end
